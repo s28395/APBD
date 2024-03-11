d840e89a0c707d4e6a75d4cce0bfc1e77a312cd7
s28395 <s28395@pjwstk.edu.pl>
2024-03-11 11:22:58 +0100

    Modyfikacja 2

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("APBD!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
