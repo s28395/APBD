95aad8deb54b72d56e509827c366bbad7441bc19
s28395 <s28395@pjwstk.edu.pl>
2024-03-11 12:42:16 +0100

    Commit feature-average

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -1,11 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        int x = 1;
-        Console.WriteLine("x="+x);
-        Console.WriteLine("Hello, World!");
+
+        int[] tab = { 1, 2, 3, 4, 5, 8, 23, 14 };
+        
+        Console.WriteLine(srednia(tab));
+    }
+
+    public static double srednia(int[] tab)
+    {
+        double suma = 0;
+        int ilosc=0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            suma += tab[i];
+            ilosc++;
+        }
+        
+        return suma/ilosc;
     }
 }
